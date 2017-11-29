c712dcaea150f7cf5d2bab0417a277365088b894
Bryant <bryantchayden@gmail.com>
2017-11-29 14:55:49 -0800

    1.0

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Main6712.java
@@ -0,0 +1,150 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+//@Disabled
+public class Main6712 extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor leftDrive2 = null;
+    private DcMotor rightDrive2 = null;
+    private DcMotor pulley = null;
+    private Servo servoRight = null;
+    private Servo servoLeft = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        // Motors labeled if your looking at the FRONT of the robot
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftDrive2 = hardwareMap.get(DcMotor.class, "left_drive2");
+        rightDrive2 = hardwareMap.get(DcMotor.class, "right_drive2");
+        pulley = hardwareMap.get(DcMotor.class, "pulley");
+        // servoLeft   = hardwareMap.get(Servo.class , "left_servo");
+        //servoRight  = hardwareMap.get(Servo.class , "right_servo");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive2.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive2.setDirection(DcMotor.Direction.REVERSE);
+        pulley.setDirection(DcMotor.Direction.REVERSE);
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double pulleyStatus = 0;
+            // double servoStatus = 0;
+            double leftPower;
+            double rightPower;
+            double pulleyPower;
+
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.left_stick_x;
+            //boolean up = gamepad1.a;
+            //boolean down =gamepad1.b;
+            //When a is pressed it goes up
+            while (gamepad1.a) {
+                pulleyStatus = -1.0;
+            }
+            //When B is pressed it goes do
+            while (gamepad1.b) {
+                //pulleyStatus =1.0;
+                // }
+                //when the buttons are not pressed the status of the motor is reset
+                while (!gamepad1.a && !gamepad1.b) {
+                    pulleyStatus = 0.0;
+                }
+                //while(gamepad1.right_bumper){
+
+                //}
+                leftPower = Range.clip(drive + turn, -1.0, 1.0);
+                rightPower = Range.clip(drive - turn, -1.0, 1.0);
+                pulleyPower = Range.clip(pulleyStatus, -1.0, 1.0);
+                // Tank Mode uses one stick to control each wheel.
+                // - This requires no math, but it is hard to drive forward slowly and keep straight.
+
+
+                // Send calculated power to wheels
+                leftDrive.setPower(leftPower);
+                rightDrive.setPower(rightPower);
+                leftDrive2.setPower(leftPower);
+                rightDrive2.setPower(rightPower);
+                pulley.setPower(pulleyPower);
+                // Show the elapsed game time and wheel power.
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+                telemetry.update();
+            }
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedside.java
@@ -0,0 +1,226 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+/**
+ * This file illustrates the concept of driving a path based on encoder counts.
+ * It uses the common Pushbot hardware class to define the drive on the  
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByTime;
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forwards, and causes the encoders to count UP.
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backwards for 24 inches
+ *   - Stop and close the claw.
+ *
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This methods assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="AutiRedSide", group="Pushbot")
+//@Disabled
+public class AutonomousRedside extends LinearOpMode {
+    private DcMotor leftDrive   = null;
+    private DcMotor rightDrive  = null;
+    private DcMotor leftDrive2  = null;
+    private DcMotor rightDrive2 = null;
+    private DcMotor pulley      = null;
+    private Servo   rightServo  = null;
+    private Servo   leftServo   = null;
+    /* Declare OpMode members. */
+    Main6712TC          robot   = new Main6712TC();   // Use a Main6712TC's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 3.875 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                                                      (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        leftDrive   = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive  = hardwareMap.get(DcMotor.class, "right_drive");
+        leftDrive2  = hardwareMap.get(DcMotor.class, "left_drive2");
+        rightDrive2 = hardwareMap.get(DcMotor.class, "right_drive2");
+        pulley      = hardwareMap.get(DcMotor.class, "pulley");
+        rightServo  = hardwareMap.get(Servo.class, "right_servo");
+        leftServo   = hardwareMap.get(Servo.class, "left_servo");
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         leftDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         rightDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+         leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+         rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                           leftDrive.getCurrentPosition(),
+                           rightDrive.getCurrentPosition(),
+                           leftDrive2.getCurrentPosition(),
+                           rightDrive2.getCurrentPosition());
+
+
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        sleep(5000);                                                 //S0:Wait 5 seconds to start
+         leftServo.setPosition(1.0);                                       // S1:Grab cube
+         rightServo.setPosition(0.0);
+        sleep(1000);                                                // pause for servos to move
+        encoderDrive(DRIVE_SPEED,  36,  36, 3.0); // S2: Drive to Glyph Box
+        encoderDrive(TURN_SPEED,   -11.780972, 11.780972, 2);     // S3: Turn towards glyph Box
+        encoderDrive(DRIVE_SPEED, 18, 18, 2);     // S4: drive into glyph box
+         leftServo.setPosition(0.5);                                      // S5: Release Cube
+         rightServo.setPosition(0.5);
+        sleep(1000);                                               // pause for servos to move
+        encoderDrive(DRIVE_SPEED, -18, -18, 2);  //S6:Backout of Glyph Box
+        encoderDrive(TURN_SPEED, -11.708972*2, 11.708972*2, 2  );  //S7 Turn around 180
+        encoderDrive(DRIVE_SPEED, -12,-12,2);    //S8:Backup and park
+        sleep(1000);                                               // pause for servos to move
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+    /*
+     *  Method to perfmorm a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget =  leftDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget =  rightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+             leftDrive.setTargetPosition(newLeftTarget);
+             rightDrive.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+             leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+             rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+             leftDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+             rightDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+             leftDrive.setPower(Math.abs(speed));
+             rightDrive.setPower(Math.abs(speed));
+             leftDrive2.setPower(Math.abs(speed));
+             rightDrive2.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                   (runtime.seconds() < timeoutS) &&
+                   ( leftDrive.isBusy() &&  rightDrive.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                                             leftDrive.getCurrentPosition(),
+                                             rightDrive.getCurrentPosition(),
+                                             leftDrive2.getCurrentPosition(),
+                                             rightDrive2.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+             leftDrive.setPower(0);
+             rightDrive.setPower(0);
+             leftDrive2.setPower(0);
+             rightDrive2.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+             leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+             rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+             leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+             rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+}
